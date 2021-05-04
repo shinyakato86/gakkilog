ca67723cf4a280230784d7da6c8b3aa6e8df2d37
shinyakato86 <fujiwarapoteto@gmail.com>
2021-05-04 23:12:06 +0900

    210504
    タイトル修正

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
   <meta name='csrf-token' content='{{ csrf_token() }}'>
-  <title>{{ config('app.name') }}</title>
+  <title>{{ config('app.name', 'GAKKI_LOG') }}</title>
   <meta name="description" content="受注案件登録システムです。">
   <meta name="format-detection" content="telephone=no">
   <meta name="robots" content="noindex">

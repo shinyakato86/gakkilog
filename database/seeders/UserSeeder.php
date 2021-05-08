b521a21291854a9cee108c242cc7c48d98da78b7
shinyakato86 <fujiwarapoteto@gmail.com>
2021-05-08 20:56:53 +0900

    210508

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,26 +17,31 @@ class UserSeeder extends Seeder
     {
       DB::table('users')->insert([
           [
+            'id' => '1',
             'name' => '明智光秀',
             'email' => 'test1@test.co.jp',
             'password' => Hash::make('password'),
           ],
           [
+            'id' => '2',
             'name' => '伊達政宗',
             'email' => 'test2@test.co.jp',
             'password' => Hash::make('password'),
           ],
           [
+            'id' => '3',
             'name' => '上杉謙信',
             'email' => 'test3@test.co.jp',
             'password' => Hash::make('password'),
           ],
           [
+            'id' => '4',
             'name' => '真田幸村',
             'email' => 'test4@test.co.jp',
             'password' => Hash::make('password'),
           ],
           [
+            'id' => '5',
             'name' => '確認用アカウント',
             'email' => 'admin@admin.co.jp',
             'password' => Hash::make('password'),

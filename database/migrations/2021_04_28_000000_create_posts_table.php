3e368007b8a95fab3dc76c805cccd20543b9b160
shinyakato86 <fujiwarapoteto@gmail.com>
2021-05-08 19:12:40 +0900

    210508

--- database/migrations/2021_04_28_000000_create_posts_table.php
+++ database/migrations/2021_04_28_000000_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->integer('category_id')->unsigned();
            /* $table->foreign('category_id')
                   ->references('id')
